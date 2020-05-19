1836aa668bd883500ae0863ebc3838631569e697
Jacob Colyvan <jacob_colyvan@live.com>
2020-05-19 22:01:10 +1000

    tidying up across site styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,12 @@ User.create(email: "stevieWonders@whyami.blind", password: "gggggg")
 
 
 Collection.create(title: "Gods", user_id: 1, description: "Because who doesn't love the ancients")
-for i in 1..10 
+for i in 1..5 
   Listing.create(title: Faker::Ancient.god, user_id: 1, collection_id: 1)
 end
 
 Collection.create(title: "Lovecraft", user_id: 1, description: "Scare your neighbours! Lovecraft!")
-for i in 1..10 
+for i in 1..5 
   Listing.create(title: Faker::Books::Lovecraft.deity, user_id: 1, collection_id: 2)
 end
 
-
-# Collection.create(title: "Animals", user_id: 1, description: "because everybody loves fur", picture: File.open('app/assets/images/animals/cod.jpg'))
